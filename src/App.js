29c0f58488708f0cddfb0b76c9e0ed796e4ea63b
Cyril <cyril.niobe@gmail.com>
2018-10-20 11:45:10 +0200

    Initial commit for App shell

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
+// Import React and Component
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// Import CSS from App.css
 import './App.css';
+// Import the Today component to be used below
+import Today from './Today/Today'
+// Import the History component to be used below
+import History from './History/History'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="App-header">
+        <header className="container">
+          <nav className="navbar"></nav>
+            <div className="navbar-brand">
+              <span className="navbar-item">HistoriCoins</span>
+            </div>
+            <div className="navbar-end">
+            <a className="navbar-item" href="https://github.com/CyrilNb/Historicoins" target="_blank" rel="noopener noreferrer">Fork me on Github!</a>
+            </div>
+          </header>
+        </div>
+        <section className="results--section">
+          <div className="container">
+            <h1>HistoriCoins is a realtime price information about <br></br>BTC, ICX and NANO.</h1>
+          </div>
+          <div className="results--section__inner">
+            <Today />
+            <History />
+          </div>
+        </section>
+
       </div>
     );
   }
