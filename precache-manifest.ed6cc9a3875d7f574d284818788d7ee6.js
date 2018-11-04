60394d60f922c7a4f65031920ecafe2263def980
Cyril <cyril.niobe@gmail.com>
2018-11-04 13:13:12 +0100

    Updates

---
+++ precache-manifest.ed6cc9a3875d7f574d284818788d7ee6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1b4c29de7d12e2bd6ba",
+    "url": "/HistoriCoins/static/js/runtime~main.e1b4c29d.js"
+  },
+  {
+    "revision": "018e9d46fe316265047e",
+    "url": "/HistoriCoins/static/js/main.018e9d46.chunk.js"
+  },
+  {
+    "revision": "47d1bb59e99047489959",
+    "url": "/HistoriCoins/static/js/1.47d1bb59.chunk.js"
+  },
+  {
+    "revision": "018e9d46fe316265047e",
+    "url": "/HistoriCoins/static/css/main.8b074a32.chunk.css"
+  },
+  {
+    "revision": "cada5485d5db110714027e6f3bf5d592",
+    "url": "/HistoriCoins/index.html"
+  }
+];
